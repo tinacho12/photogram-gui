baa075a0da322e7367b2947bf2efa984a6d82a3c
tinacho12 <146214538+tinacho12@users.noreply.github.com>
2023-11-22 17:39:51 +0000

    Added Users#Show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,18 @@ class UsersController < ApplicationController
 
     render({ :template => "user_templates/index" })
   end
+
+  def show
+    url_username = params.fetch("username")
+
+    matching_usernames = User.where({ :username => url_username })
+    
+    @the_user = matching_usernames.first
+
+    if @the_user == nil
+      redirect_to("/404")
+    else
+      render({ :template => "user_templates/show" })
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
   get("/users", { :controller => "users", :action => "index" })
-  
+
+  get("/users/:username", { :controller => "users", :action => "show" })
+
 end
